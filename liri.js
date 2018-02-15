f6dd9724657401d58697981c0e0e4ddc851af8ea
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-02-15 02:09:05 -0800

    finish addmovie

--- liri.js
+++ liri.js
@@ -21,17 +21,19 @@ var dataInput = inputs[1];
 	getTweets();
 	break;
 	case "spotify-this-song":
-	for (i=2; i<inputs.length;i++) {
+	for (i = 2; i < inputs.length; i++) {
 		dataInput = dataInput + " " + inputs[i];
 	}
 	getSongs();
 	break;
 	case "movie-this":
+	for (i = 2; i < inputs.length; i++) {
+	dataInput = dataInput + " " + inputs[i];
+	}
+	getMovies();
 	break;
 	case "do-what-it-says":
 	break;
-	default:
-	block;
 	}
 
 
@@ -44,7 +46,7 @@ var dataInput = inputs[1];
             access_token_secret: APIKey.access_token_secret
         });
 
-// HW require 20 tweets. Just change the count to 20... I don't have that much tweets
+// HW require 20 tweets. Just change the count to 20... I don't have that much tweets in my personal twitter
         var params = { ouyangtt: 'nodejs', count: 10 };
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
                 if (!error) {
@@ -63,60 +65,93 @@ var dataInput = inputs[1];
 
  function getSongs() {
 
-var spotify = new Spotify({
-  id: "91895b2c34454f0f89a8a98de25b8505",
-  secret: "e0ff58385a4c4dc191a40829ac2b9b91"
-});
+		var spotify = new Spotify({
+		  id: "91895b2c34454f0f89a8a98de25b8505",
+		  secret: "e0ff58385a4c4dc191a40829ac2b9b91"
+		});
  
 
-if (!dataInput) {
-	dataInput = "The Sign";
-}
+		if (!dataInput) {
+			dataInput = "The Sign";
+		}
 
-console.log("dataInput:" + dataInput);
+			console.log("dataInput:" + dataInput);
  
-spotify.search({ type: 'track', query: dataInput }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+			spotify.search({ type: 'track', query: dataInput }, function(err, data) {
+			  if (err) {
+			    return console.log('Error occurred: ' + err);
+			  }
+			        console.log('Artist Name: '+data.tracks.items[0].artists[0].name+'\n');
+			        console.log('Song Name: '+data.tracks.items[0].name+'\n');
+			        console.log('Spotify URL: '+data.tracks.items[0].artists[0].external_urls.spotify+'\n');
+			        console.log('Album Name: '+data.tracks.items[0].album.name+'\n');
+			});
+	};
 
-                    console.log('Artist Name: '+data.tracks.items[0].artists[0].name+'\n');
-                    console.log('Song Name: '+data.tracks.items[0].name+'\n');
-                    console.log('Spotify URL: '+data.tracks.items[0].artists[0].external_urls.spotify+'\n');
-                    console.log('Album Name: '+data.tracks.items[0].album.name+'\n');
 
-     
-  
+	function getMovies() {
+
+
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t=" + dataInput + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!error && response.statusCode === 200) {
+
+     if (!dataInput) {
+			dataInput = "mr nobody";
+		}
+
+	    console.log("dataInput:" + dataInput);
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Year: " + JSON.parse(body).Year);
+        console.log("Rating: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+
+
+
+    }
 });
-};
-            // spotify.search({ type: 'track', query: dataInput }, function(err, results) {
-            //     // if there is an error console log it
-            //     if ( err ) {
-            //         console.log('Error occurred: ' + err);
-            //         console.log('Error: '+err+'\n');
-            //         return;
-            //     }
-            //     // if there are results
-            //     console.log(results.tracks);
 
-                // if (results.tracks.items[0].artists[0].name) {
-                //     // tell the user the results
-                //     console.log('Artist Name: '+results.tracks.items[0].artists[0].name);
-                //     console.log('Song Name: '+results.tracks.items[0].name);
-                //     console.log('Spotify URL: '+results.tracks.items[0].artists[0].external_urls.spotify);
-                //     console.log('Album Name: '+results.tracks.items[0].album.name);
-                //     console.log('Artist Name: '+results.tracks.items[0].artists[0].name+'\n');
-                //     console.log('Song Name: '+results.tracks.items[0].name+'\n');
-                //     console.log('Spotify URL: '+results.tracks.items[0].artists[0].external_urls.spotify+'\n');
-                //     console.log('Album Name: '+results.tracks.items[0].album.name+'\n');
-                
-                // // if there are no results
-                // } else {
-                //     // tell the user to choose a new song
-                //     console.log('We did not find any results for that song.');
-                //     console.log('We did not find any results for that song.\n');
-                // } 
- //            });
+// request('http://www.omdbapi.com/?t='+dataInput+'&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
+//                 // if there is no error and a status of 200
+//                 if (!error && response.statusCode == 200) {
+//                     // convert the result string to actionable JSON
+//                     body = JSON.parse(body);
+//                     // console log the specific results that we want
+//                     // Title
+//                     console.log('Title: '+body.Title);
+//                     // Year
+//                     console.log('Year: '+body.Year);
+//                     // IMDB Rating
+//                     console.log('IMDB Rating: '+body.imdbRating);
+//                     // Country
+//                     console.log('Country: '+body.Country);
+//                     // Language
+//                     console.log('Language: '+body.Language);
+//                     // Plot
+//                     console.log('Short Plot: '+body.Plot);
+//                     // Actors
+//                     console.log('Actors: '+body.Actors);
+//                     // Rotten Tomatoes Rating
+//                     console.log('Rotten Tomatoes Rating: '+body.tomatoUserRating);
+//                     // Rotton Tomatoes UrL
+//                     console.log('Rotton Tomatoes URL: '+body.tomatoURL);
+//                 // if there is an error
+//                 } else {
+//                     // tell the user the error
+//                     console.log('Error: '+error);
+//                 }
+//             })
 
- // };
-   
+
+
+
+
+
+	};
+            
